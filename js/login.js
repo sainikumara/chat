babf444276599196e3705189217282860288659e
Saini Kumara <saini@iki.fi>
2017-02-28 07:41:07 +0200

    check nickname validity

--- js/login.js
+++ js/login.js
@@ -1,35 +1,45 @@
 $(document).ready(function () {
+  try {
+    window.localStorage
+  } catch (e) {
+    window.location.href = '/error.html';
+  }
+
   $('form').submit(function () {
 
     var $nick_input = $('#nick');
     var nick = $nick_input.val();
 
-    if (nick == '') {
+    // Check nickname validity
+    var nickValidityChecker = function () {
+      if (typeof nick === 'string' && 1 <= nick.length && nick.length <= 20 && !/[^a-z0-9]/i.test(nick)) {
+        return 1;
+      }
+    }
+
+    if (nickValidityChecker() !== 1) {
       $('input[type="text"],input[type="nickname"]').css('border', '2px solid red');
-      alert('Choose a nickname to enter');
+      alert('You can use letters a-z, A-Z and numbers 0-9.\n' +
+        'Minimum length is 1 character and maximum length 20 characters.');
     } else {
-      //Sending nick to server to check if it is already in use
+      //Send nick to server to check if it is already in use
       var socket = io();
       socket.emit('nick', nick);
 
       var approveNick = function (reply) {
         if (reply === 'isOk') {
-          // If session storage is supported, save nickname and proceed
-          if (typeof Storage !== 'undefined') {
-            sessionStorage.setItem('nickname', nick);
-            window.location.href = 'channel.html';
-          }
-          else {
-            // FIXME: Sorry, no local storage support
-          }
+          localStorage.setItem('nickname', nick);
+          window.location.href = '/';
         } else {
           $('input[type="text"],input[type="nickname"]').css('border', '2px solid red');
           alert('Sorry, nickname already in use. Try another one.');
         }
       };
-      socket.on('nickOK', approveNick);
 
+      // Receive the reply from the server
+      socket.on('nickOK', approveNick);
     }
     return false;
   });
+
 });
